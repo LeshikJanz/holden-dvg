cff22e845801853e6c5c65dbd42756422f172d4a
leshikjanz <alex.tereshko@softeq.com>
2018-02-28 21:41:30 +0300

    refactor: change phone number

--- components/Callon/CallonComponent.js
+++ components/Callon/CallonComponent.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import css from 'services/cssService';
 import styles from 'template/components/Callon/CallonComponent.scss';
-import get from 'lodash/get';
 
-const CallonComponent = ({ product }) => (
-  <div styleName="block">
-    <div styleName="component">
-      <div styleName="component-callon">
-        Call your DVG dealership on
-      </div>
-      <div styleName="component-number">
-        <div styleName="items-row">
-          <div styleName="call-icon" />
-          <a href={`tel:${get(product, 'DealerLocation.phone', 'na')}`}>{get(product, 'DealerLocation.phone', 'na')}</a>
+const PHONE_NUMBER = "08 6555 7009"
+
+const CallonComponent = ({ product }) => {
+  return (
+    <div styleName="block">
+      <div styleName="component">
+        <div styleName="component-callon">
+          Call your dealership on
+        </div>
+        <div styleName="component-number">
+          <div styleName="items-row">
+            <div styleName="call-icon" />
+            <a href={`tel:${PHONE_NUMBER}`}>{PHONE_NUMBER}</a>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+}
 
 export default css(CallonComponent, styles);
